e840f9c11f6bb60f9cf346d6c4f06814ae9ec348
Rahul Sumbly <70894220+Rahul-s-007@users.noreply.github.com>
2023-09-26 14:27:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# Take user input
+num1 = st.text_input("Enter the first number:")
+num2 = st.text_input("Enter the second number:")
+operation = st.radio("Select an operation:", ("Add", "Subtract", "Multiply", "Divide"))
+
+if (num1!="" and num2!=""):
+  if st.button("Calculate"):
+      if operation == "Add":
+          result = float(num1) + float(num2)
+      elif operation == "Subtract":
+          result = float(num1) - float(num2)
+      elif operation == "Multiply":
+          result = float(num1) * float(num2)
+      elif operation == "Divide":
+          result = float(num1) / float(num2)
+      st.write("Result:", result)
